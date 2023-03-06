381e460cff7c7aa216e607ca07ac2d9c7811f66c
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2023-03-06 07:17:55 +0530

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.sanjay-kr-commit:tanoshi-source-api:main-SNAPSHOT")
+    implementation("com.github.sanjay-kr-commit:tanoshi-source-api:0.1")
     implementation("com.squareup.okhttp:okhttp:2.7.5")
     implementation("org.jsoup:jsoup:1.15.3")
     testImplementation(kotlin("test"))
